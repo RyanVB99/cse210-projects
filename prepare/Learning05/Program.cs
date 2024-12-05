09e18364861101212ec216365676d97fbc01a704
RyanVBSpheres <ryanvbodily@gmail.com>
2024-12-05 14:43:42 +1300

    Completed inheritance learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        string studentName = "John";
+        string topic = "Algebra";
+        string textbookSection = "Chapter 3";
+        string problems = "1-10";
+
+        MathAssignment mathAssignment = new MathAssignment(studentName, topic, textbookSection, problems);
+
+    Console.WriteLine(mathAssignment.GetSummary());
+    Console.WriteLine(mathAssignment.GetHomeworkList());
+
+    string title = "Writing Assignment 7";
+    WritingAssignment writingAssignment = new WritingAssignment(studentName, topic, title);
+
+    Console.WriteLine(writingAssignment.GetSummary());
+    Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }

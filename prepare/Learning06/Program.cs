bdffd3281d36a7cdc32861a821415b9ca1449ea4
RyanVBSpheres <ryanvbodily@gmail.com>
2024-12-10 11:08:47 +1300

    Completed learning activity

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Circle("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 5));
+        shapes.Add(new Square("Green", 5));
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Area of {shape.GetType().Name} is {shape.GetArea()}");
+        }
+        
     }
 }
